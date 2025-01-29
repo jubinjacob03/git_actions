c610fc52decc6726914505ff2145ba59a5740122
jubinjacob03 <jr.jubin03@gmail.com>
2025-01-29 15:07:58 +0530

    Template changes

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,22 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <div>
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </div>
+        <div>
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        </div>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Git + GitHub</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          Click me !! : {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        This Template has been edited !!
       </p>
     </>
   )
