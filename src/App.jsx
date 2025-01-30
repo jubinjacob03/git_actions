422bef50a71024382c749da8f37c26ee2324675e
jubinjacob03 <jr.jubin03@gmail.com>
2025-01-30 17:55:07 +0530

    Initial commit ( with all changes )

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <div>
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </div>
+        <div>
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </div>
+      </div>
+      <h1>Git + GitHub</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          Click me !! : {count}
+        </button>
+
+      </div>
+      <p className="read-the-docs">
+        Attempting a merge conflict !!
+      </p>
+    </>
+  )
+}
+
+export default App
